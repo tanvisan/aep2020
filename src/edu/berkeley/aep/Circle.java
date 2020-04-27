297710a5bf7a8dba803c0e4f2a741b5de138d197
Tanvi S <tanvi.s@berkeley.edu>
2020-04-26 23:19:52 -0700

    Class Circle MMethod Area Test

--- src/edu/berkeley/aep/Circle.java
+++ src/edu/berkeley/aep/Circle.java
@@ -9,4 +9,8 @@ public class Circle {
     public Circle(double radius) {
         this.radius = radius;
     }
+
+    public double area() {
+        return Math.PI * (radius * radius);
+    }
 }
--- test/edu/berkeley/aep/CircleTest.java
+++ test/edu/berkeley/aep/CircleTest.java
@@ -5,9 +5,17 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class CircleTest {
+    private static final double DELTA = 1e-15;
+
     @Test
     public void defineACircleWithRadius(){
         var circle = new Circle(6);
+    }
+
+    @Test
+    public void AreaofZeroCirlceIsZero(){
+        var circle = new Circle(0);
+        assertEquals(0, circle.area(), DELTA);
     }
 }
 
