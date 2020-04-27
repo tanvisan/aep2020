ac5b6573814bc8f13c4d4a74e1c9fae512cfdaee
Tanvi S <tanvi.s@berkeley.edu>
2020-04-27 00:37:35 -0700

    Area of incircle less than area of triangle

--- test/edu/berkeley/aep/TriangleTest.java
+++ test/edu/berkeley/aep/TriangleTest.java
@@ -2,6 +2,7 @@ package edu.berkeley.aep;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class TriangleTest {
     @Test
@@ -34,5 +35,16 @@ public class TriangleTest {
         var triangle = new Triangle(5, 12, 13);
         var circle = new Circle(65/10);
         assertEquals(circle, triangle.outcircle());
+    }
+
+    @Test
+    public void AreaofIncircleLessThanAreaOfTriangle() {
+        var triangle = new Triangle(5, 12, 13);
+        var incircle = triangle.incircle();
+        var areaTriangle = triangle.area();
+        var areaIncircle = incircle.area();
+        var circleObj = new Circle(2);
+        assertTrue(areaIncircle < areaTriangle);
+        assertEquals(incircle, circleObj);
     }
 }
